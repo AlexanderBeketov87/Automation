54aa6da77f37792faf47366287745d305417d002
Александр <153617878+AlexanderBeketov87@users.noreply.github.com>
2024-03-15 12:21:08 +0300

    Update lesson_2_task_10.py

--- lesson2/lesson_2_task_10.py
+++ lesson2/lesson_2_task_10.py
@@ -2,14 +2,14 @@ def bank(X, Y):
     
     total = X
     
-    percent = 0.05  # 5% годовых
+    percent = 0.10  # 5% годовых
     
     for _ in range(Y):
         total += total * percent
     
     return total
 
-X = 500  # Размер вклада в рублях
-Y = 3     # Срок вклада в годах
+X = 1000  # Размер вклада в рублях
+Y = 5     # Срок вклада в годах
 result = bank(X, Y)
 print("Сумма на счету пользователя через", Y, "года:", result, "рублей")
